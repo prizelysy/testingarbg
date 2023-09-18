15ca7bb04b99f0038151b6a15065956512ef0192
prizelysy <prizelyff2103@gmail.com>
2023-09-18 11:24:59 +0700

    first commit

---
+++ js/read-more.js
@@ -0,0 +1,19 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const noteLink = document.getElementById("read-more-link");
+//     const noteParagraph = document.getElementById("read-more");
+
+//     // Function to toggle the visibility of the note
+//     function toggleNote() {
+//         if (noteParagraph.style.display === "none" || noteParagraph.style.display === "") {
+//             noteParagraph.style.display = "block"; // Show the note
+//         } else {
+//             noteParagraph.style.display = "none"; // Hide the note
+//         }
+//     }
+
+//     // Add a click event listener to the note link
+//     noteLink.addEventListener("click", function (e) {
+//         e.preventDefault(); // Prevent the link from navigating
+//         toggleNote(); // Call the toggle function
+//     });
+// });
